1336a0cf5d9d6e980212c8cdfd1dc3eb3b228dd9
Malaiz <112379239+Malaiz@users.noreply.github.com>
2022-09-12 17:28:58 +0200

    Add files via upload

---
+++ mixed-messages/script.js
@@ -0,0 +1,19 @@
+const formation = "defensive";
+
+switch (formation) {
+    case "defensive":
+      console.log("4-3-3");
+      break;
+    case "highPressing":
+        console.log("4-5-2");
+      break;
+    case "expansive":
+        console.log("4-4-2");
+      break;
+    case "overloadingMidfield":
+        console.log("3-2-5");
+      break;
+    default:
+        console.log("4-4-3");
+    
+  }
